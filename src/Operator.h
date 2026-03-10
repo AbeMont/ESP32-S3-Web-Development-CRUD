b85f58ae271b21d92c60b44b984f71195fdcd9e3
abemont <abraham.montoya1990@gmail.com>
2026-03-10 17:45:46 -0500

    Creates implementation file for for Operator.h

--- src/Operator.h
+++ src/Operator.h
@@ -1,73 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
 #include <optional>
+#include<WString.h>
 
 class Operator {
-private:
-    /* data */
-public:
-    int id;
-    String name;
-    String weapon;
-    String metalGear;
+    private:
+        /* data */
+    public:
+        int id;
+        String name;
+        String  weapon;
+        String  metalGear;
 
-    Operator(int id, String name, String weapon, String metalGear){
-        this->id = id;
-        this->name = name;
-        this->weapon = weapon;
-        this->metalGear = metalGear;
-    }
-    // Default values
-    Operator() : id(0), name("Jack"), weapon("Famas"), metalGear("None") {}
+        Operator(int id, String name, String  weapon, String  metalGear);
+
 };
 
-Operator* getOperatorbyId(std::vector<Operator> &operators, int targetId) {
-    auto it = std::find_if(operators.begin(), operators.end(), [targetId](const Operator &obj) {
-        return obj.id == targetId;
-    });
-    // Check if a matching object was found
-    if (it != operators.end()) {
-        // Return the pointer (Operator*) to the found object
-        return &(*it);
-    } else {
-        // Return nullptr if not found
-        return nullptr;
-    }
-}
+Operator* getOperatorbyId(std::vector<Operator> &operators, int targetId);
 
-void removeOperatorById(std::vector<Operator> &operators, int targetId) {
-    // std::remove_if shifts elements to be kept to the front and returns an iterator 
-    // to the new logical end of the remaining elements.
-    auto it = std::remove_if(operators.begin(), operators.end(), [targetId](const Operator &obj) { 
-        return obj.id == targetId; 
-    });
-
-    // vector::erase then removes the elements from the new logical end to the actual end.
-    operators.erase(it, operators.end());
-}
+void removeOperatorById(std::vector<Operator> &operators, int targetId);
 
 Operator* updateOperatorById(std::vector<Operator> &operators, 
     int targetId, 
     String updatedName,
     String updatedWeapon,
-    String updatedMetalgear) {
-    // Use reference (&)in &obj to modify the actual object in the vector
-    auto operatorObj = std::find_if(operators.begin(), operators.end(), [targetId](const Operator &obj){
-        return obj.id == targetId;
-    });
-
-    if (operatorObj != operators.end()) {
-        operatorObj->name = updatedName;
-        operatorObj->weapon = updatedWeapon;
-        operatorObj->metalGear = updatedMetalgear;
-        // Return the pointer (Operator*) to the updated object
-        return &(*operatorObj);
-    } else {
-        return nullptr;
-    }
-}
+    String updatedMetalgear);
 
 
 // Struct Method
